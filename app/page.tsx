1a5e269e273c035b3eea1466d60155e261c5a98b
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-04-21 19:12:54 -0300

    Basic structure project

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import { Inter } from 'next/font/google'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+          {/* Sing in section */}
+          <div className="w-3/5 p-5">
+            <p>Sing In</p>
+          </div>
+          
+          {/* Sing up section */}
+          <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+            <p>Sing Up</p>
+          </div>   
+        </div>
+      </main>
     </div>
 
   )
