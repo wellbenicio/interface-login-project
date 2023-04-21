1a86369c3a96ac5ae1485f64717ca7603993a768
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-04-21 19:31:11 -0300

    Implementation of the sing up section

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,10 @@ export default function Home() {
           
           {/* Sing up section */}
           <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
-            <p>Sing Up</p>
+            <h2 className="text-3xl font-bold mb-2">Hello, Friend!</h2>
+            <div className="border-2 w-10 border-white inline-block mb-2"></div>
+            <p className="mb-10">Fill up personal information and start journey with us.</p>
+            <a href="#" className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500">Sing Up</a>
           </div>   
         </div>
       </main>
