717cd4fb5727ba8f69597ce6e802b28af0378874
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-04-21 18:58:06 -0300

    Intalling packages into project.

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+    </div>
+
+  )
+}
+
+// 
